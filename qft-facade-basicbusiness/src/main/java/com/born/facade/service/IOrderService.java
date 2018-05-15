dfdc5c412450624fb858b5a692f5e018e5ec0863
lijie <498959412@qq.com>
2018-05-15 14:40:13 +0800

    订单相关服务

--- qft-facade-basicbusiness/src/main/java/com/born/facade/service/IOrderService.java
+++ qft-facade-basicbusiness/src/main/java/com/born/facade/service/IOrderService.java
@@ -1,5 +1,12 @@
 package com.born.facade.service;
-
+/**
+ * 
+* @ClassName: IOrderService 
+* @Description: 订单相关服务 
+* @author lijie 
+* @date 2018年5月15日 下午2:39:50 
+*
+ */
 public interface IOrderService {
 
 }
